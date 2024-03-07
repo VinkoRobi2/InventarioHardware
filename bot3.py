6c3cd129f153c4db0223533b2d13af377f53e8e7
VinkoRobi2 <161661761+VinkoRobi2@users.noreply.github.com>
2024-03-07 12:18:51 -0500

    Ansley

--- bot3.py
+++ bot3.py
@@ -10,16 +10,22 @@ bot_activado = True
 ContrasenaG = 1012020
 
 
+
 @bot.message_handler(func=lambda message: True)
 def on_any_message(message):
-    global bot_activado
-    if bot_activado:
-        markup = InlineKeyboardMarkup(row_width=2)
-        item1 = InlineKeyboardButton('Si, deseo crear una cuenta', callback_data='Si1')
-        item2 = InlineKeyboardButton("No en este momento", callback_data="later")
-        markup.add(item1, item2)
-        bot.reply_to(message.chat.id, '¡Hola! Soy InventarioHardware, tu bot para conocer el inventario. ¿Deseas crear una cuenta?', reply_markup=markup)
-        bot_activado = False
+    telegram_id = message.from_user.id
+    if user_exists(telegram_id):
+        user_recognize(message, telegram_id)
+    else:
+        global bot_activado
+        if bot_activado:
+            markup = InlineKeyboardMarkup(row_width=2)
+            item1 = InlineKeyboardButton('Si, deseo crear una cuenta', callback_data='Si1')
+            item2 = InlineKeyboardButton("No en este momento", callback_data="later")
+            markup.add(item1, item2)
+            bot.reply_to(message, '¡Hola! Soy InventarioHardware, tu bot para conocer el inventario. ¿Deseas crear una cuenta?', reply_markup=markup)
+            bot_activado = False
+
 
 
     
@@ -31,12 +37,18 @@ def handle_button_click(call):
     elif call.data == 'later':
         Regresar(call.message)
 
+
+
+
+
 def guardar_nombre(message):
     global name
     name = message.text
     name = unidecode(name)
     bot.send_message(message.chat.id, "Ahora ingresa tu Apellido:")
     bot.register_next_step_handler(message, guardar_apellido)
+
+
 
 def guardar_apellido(message):
     global lastname
@@ -52,30 +64,111 @@ def Regresar(message):
      bot.send_message(message.chat.id,"!Listo si me vuelves a necesitar, no dudes en escribirme")
      bot_activado = False
 
+
+
 def validar_contrasena(message, name):
     global Contra
+    global telegram_id
     Contra = message.text
     Contra = unidecode(Contra)
-    if int(ContrasenaG) == int(Contra):  # Comparar la contraseña con ContrasenaG
+    if int(ContrasenaG) == int(Contra):  
         bot.send_message(message.chat.id, '¡Contraseña Correcta!')
-        telegram_id = message.chat.id  # Obtener automáticamente el ID de Telegram del usuario
-        guardar_datos_en_archivo(name, lastname, telegram_id)  # Guardar los datos en el archivo directamente
+        telegram_id = message.chat.id  
+        guardar_datos_en_archivo(name, lastname, telegram_id)  
         bot.send_message(message.chat.id, '¡Tus datos se han guardado correctamente!')
     else:
         bot.send_message(message.chat.id, "¡Contraseña incorrecta! Por favor intenta nuevamente.")
-        bot.register_next_step_handler(message, guardar_apellido)  # Volver a solicitar la contraseña
+        bot.register_next_step_handler(message, guardar_apellido)  
+
+
+
 
 def guardar_datos_en_archivo(name, lastname, telegram_id):
-    with open('usuarios.txt', 'w') as file:
-        file.write(f"Nombre: {lastname}\n")  # Cambio de name a lastname
-        file.write(f"Apellido: {name}\n")    # Cambio de lastname a name
-        file.write(f"ID de Telegram: {telegram_id}\n")
-        file.write("\n")
+    with open("usuarios.txt", "r+") as archivo:
+        datos = json.load(archivo)       
+        nuevo_elemento = {
+            "Apellido": lastname,
+            "telegram": telegram_id,
+            "Nombre": name
+        }        
+        datos.append(nuevo_elemento)
+        archivo.seek(0)  
+        json.dump(datos, archivo, indent=4)  
+    usuarios.clear()  
+    usuarios.extend(datos) 
+
+
+
+
+def Opciones(message):
+    markup = InlineKeyboardMarkup(row_width=1)
+    item1 = InlineKeyboardButton('Ver Lista',callback_data='VerLista')
+    item3 = InlineKeyboardButton('Agregar Item', callback_data='AgregarItem')
+    item4 = InlineKeyboardButton('Eliminar Item', callback_data='EliminarItem')
+    item2 = InlineKeyboardButton('Salir', callback_data='Salir')
+    markup.add(item1,item3,item4,item2)
+    for item in usuarios:
+        if item['telegram'] == message.from_user.id:
+            usuario = f"{item['Nombre']} {item['Apellido']}"
+            mensaje = f"Bienvenido de vuelta, {usuario}! ¿Qué deseas hacer?"
+            bot.send_message(message.chat.id, mensaje, reply_markup=markup)
+            return
+
+    bot.send_message(message.chat.id, '¡Bienvenido! ¿Deseas crear una cuenta?', reply_markup=markup)
+
+    
+
+
+def user_recognize(message, telegram_id):
+    for item in usuarios:
+        if item['telegram'] == telegram_id:
+            Opciones(message) 
+            return
+    on_any_message(message)
+
+
+
+def user_exists(telegram_id):
+    for item in usuarios:
+        if item['telegram'] == telegram_id:
+            return True
+    return False
+
+
+
+
+def cargar_usuarios():
+    try:
+        with open("usuarios.txt", "r") as archivo:
+            return json.load(archivo)
+    except FileNotFoundError:
+        return []
+
+usuarios = cargar_usuarios()
+
+
+
+
+
+
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+      
+      telegram_id = message.from_user.id
+      if user_exists(telegram_id):
+          print(telegram_id)
+          user_recognize(message, telegram_id)
+      else:
+          print(telegram_id)
+          on_any_message(message)
 
 
 
 
 bot.polling()
+
+
 
 
 
