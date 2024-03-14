a5b9fce7f9cb98d461348e1558e5c9968e548433
VinkoRobi2 <161661761+VinkoRobi2@users.noreply.github.com>
2024-03-14 10:44:47 -0500

    Ansley

--- FETCH.js
+++ FETCH.js
@@ -1,15 +1,14 @@
 
 const fs = require('fs');
 
-// Ruta del archivo .txt a leer
+
 const filePath = 'InventarioHardware/items.txt';
 
-// Leer el archivo .txt de forma asíncrona
+
 fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return;
     }
-    // Aquí puedes trabajar con el contenido del archivo
     console.log(data)
 });
